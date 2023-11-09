e23b29da5943f85fa530eb19a9e2bf620fbd6e30
JamieIsMe <120991704+JamieIsMe@users.noreply.github.com>
2023-11-09 11:45:35 +0000

    Enemy Detection Comment

--- MicroProcessorProject/src/main.c
+++ MicroProcessorProject/src/main.c
@@ -196,6 +196,7 @@ int main()
 			}
 			
 		}		
+		//Checks If User Is Inside The Enemy
 		if (isInside(enemyx,enemyy,50,5,x,y) || isInside(enemyx,enemyy,50,5,x+10,y) || isInside(enemyx,enemyy,50,5,x,y+9) || isInside(enemyx,enemyy,50,5,x+10,y+9)){
 			//Sets The Character Coords Back To Default
 			x = 64;
